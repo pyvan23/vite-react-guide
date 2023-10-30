1ca5c13c2d468f87b6850c7adf2e7bcfa44340d0
Ivan <85912778+pyvan23@users.noreply.github.com>
2023-10-30 19:44:11 +0000

    fetc

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
-export function App (){
+export function App({ offset = 20, limit = 20 }) { 
 
+    const [poke, setPoke] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
 
-useEffect(()=>{
+    const URL = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}` 
 
-    fetch()
+    useEffect(() => {
+        setLoading(true)
+        fetch(URL)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.statusText}`)
+                }
+                return response.json()
+            })
+            .then(data => {
+                setPoke(data.results)
+                setLoading(false)
+            })
+            .catch(err => {
+                setError(err.message)
+                setLoading(false)
+            })
+    }, [URL])
 
-},[])
-
-return <h1>APP </h1>
-
+    return (
+        <div>
+            <h1>APP</h1>
+            {loading && <p>Cargando...</p>}
+            {error && <p>Error: {error}</p>}
+            <ul>
+                {poke.map((pokemon, index) => (
+                    <li key={index}>{pokemon.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
